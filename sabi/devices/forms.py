cf3b5dc5a01189160e7cab17a7a1f25f9f69cdf2
Stefan Andrei <stefan.andrei@virginmedia.co.uk>
2019-02-19 11:50:10 +0000

    adding Edit functionality inside the list db records view

--- sabi/devices/forms.py
+++ sabi/devices/forms.py
@@ -19,5 +19,17 @@ class DelForm(FlaskForm):
     id = IntegerField('Id Number of Device to Remove:')
     submit = SubmitField('Remove Device')
 
+class UpdateForm(FlaskForm):
+
+    #validators = [DataRequired(message='This field is required!'), IPAddress(message='Must be an IP Address. Eg 192.168.1.11')
+
+    hostname = StringField('Hostname of Device:',validators=[DataRequired(message='Hostname field is required!')])
+    nasid = StringField('NASID of Device:',validators=[DataRequired()])
+    ip = StringField('IP Address of Device:',validators=[IPAddress(message='IP Address of Device field has to be an IP Address. Eg: 192.168.0.11 or 1.1.1.1 127.162.10.1, etc')])
+    gateway = StringField('Gateway of Device:',validators=[DataRequired()])
+    wan = StringField('WAN of Device:',validators=[DataRequired()])
+    location = StringField('Location of Device:',validators=[DataRequired()])
+    submit = SubmitField('Update Device',validators=[DataRequired()])
+
 
 
--- sabi/devices/views.py
+++ sabi/devices/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from sabi import db
 from sabi.models import Device
-from sabi.devices.forms import AddForm, DelForm
+from sabi.devices.forms import AddForm, DelForm, UpdateForm
 from sqlalchemy.exc import IntegrityError
 
 devices_blueprint = Blueprint('devices', __name__, template_folder = 'templates')
@@ -41,7 +41,7 @@ def list():
     return render_template('list.html', devices=devices)
 
 
-
+# Delete Records separate page
 @devices_blueprint.route('/delete', methods=['GET', 'POST'])
 def delete():
 
@@ -56,10 +56,42 @@ def delete():
         return redirect(url_for('devices.list'))
     return render_template('delete.html',form=form)
 
-# Code for deleting records inside list records table
+# Delete records 
 @devices_blueprint.route('/<int:device_id>/delete', methods=['POST'])
 def delete_device(device_id):
     device_id = Device.query.get_or_404(device_id)
     db.session.delete(device_id)
     db.session.commit()
     return redirect(url_for('devices.list'))
+
+# Update records
+@devices_blueprint.route('/<int:device_id>/update', methods=['GET', 'POST'])
+def update(device_id):
+
+    device = Device.query.get_or_404(device_id)
+
+    form = UpdateForm()
+
+    if form.validate_on_submit():
+        device.hostname = form.hostname.data
+        device.nasid = form.nasid.data
+        device.ip = form.ip.data
+        device.gateway = form.gateway.data
+        device.wan = form.wan.data
+        device.location = form.location.data
+
+        db.session.commit()
+
+        return redirect(url_for('devices.list', device_id=device.id))
+
+    elif request.method == 'GET':
+        form.hostname.data = device.hostname
+        form.nasid.data = device.nasid
+        form.ip.data = device.ip
+        form.gateway.data = device.gateway
+        form.wan.data = device.wan
+        form.location.data = device.location
+    return render_template('update.html',form=form)
+
+
+
